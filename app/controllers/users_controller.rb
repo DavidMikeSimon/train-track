ac4b3f5905b4bc1833ff521457a52081b0b46a4a
DavidMikeSimon <david.mike.simon@gmail.com>
2010-06-12 17:40:13 +0000

    Only displaying a nav link to the user index page if current user is an admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
   hobo_user_controller
 
   auto_actions :all
+  
+  def index
+    permission_denied("Permission Denied") unless current_user.administrator?
+    hobo_index
+  end
 
   def create
     hobo_create do
