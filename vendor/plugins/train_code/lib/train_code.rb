60af6e31c0f11fe115c19cd95c590f462c8c6fea
DavidMikeSimon <david.mike.simon@gmail.com>
2010-07-10 23:30:22 +0000

    Implemented more of the TrainCode plugin

--- vendor/plugins/train_code/lib/train_code.rb
+++ vendor/plugins/train_code/lib/train_code.rb
@@ -1,9 +1,25 @@
-class TrainCode
+module TrainCode
+  DOMAIN = 9**4 # No input number this large or larger can fit in 4 digits of base 9
+  OFFSETS = [0, 1527, 4872, 3456] # Added to each field modulo DOMAIN
+  
   def self.encode(i)
-    i
+    raise TrainCodeError.new("Cannot encode %s, it's not an Integer") unless i.is_a? Integer
+    raise TrainCodeError.new("Cannot encode %u, it's larger than %u" % [i, DOMAIN]) if i >= DOMAIN
+    raise TrainCodeError.new("Cannot encode %u, it's negative") if i < 0
+    
+    s = ""
+    (0..3).each do |n|
+      s += "-" if n > 0
+      s += ("%04u" % ((i+OFFSETS[n]) % DOMAIN).to_s(9))
+    end
+    return s
   end
   
   def self.decode(s)
-    s
+    parts = s.split("-")
+    return parts[0].to_i(9)
   end
+end
+
+class TrainCodeError < RuntimeError
 end
--- vendor/plugins/train_code/test/train_code_test.rb
+++ vendor/plugins/train_code/test/train_code_test.rb
@@ -4,4 +4,46 @@ class TrainCodeTest < ActiveSupport::TestCase
   test "can encode a number and get it back after decoding" do
     assert_equal 1234, TrainCode.decode(TrainCode.encode(1234))
   end
+  
+  test "cannot encode a number larger than or equal to TrainCode.DOMAIN" do
+    assert_raise TrainCodeError do TrainCode.encode(TrainCode::DOMAIN) end
+    assert_raise TrainCodeError do TrainCode.encode(TrainCode::DOMAIN + 55) end
+    assert_nothing_raised do TrainCode.encode(TrainCode::DOMAIN-1) end
+  end
+  
+  test "cannot encode negative numbers" do
+    assert_raise TrainCodeError do TrainCode.encode(-1) end
+    assert_nothing_raised do TrainCode.encode(0) end
+  end
+  
+  test "cannot encode non-integers" do
+    assert_raise TrainCodeError do TrainCode.encode(3.5) end
+    assert_raise TrainCodeError do TrainCode.encode("45") end
+  end
+  
+  test "encoded number is 16 digits separated by dashes into 4 4-digit sections" do
+    assert_match /^\d{4}-\d{4}-\d{4}-\d{4}$/, TrainCode.encode(1234)
+    assert_match /^\d{4}-\d{4}-\d{4}-\d{4}$/, TrainCode.encode(1)
+  end
+  
+  test "encoded number's first section is the input number in base 9" do
+    assert_match /^#{1234.to_s(9)}-/, TrainCode.encode(1234)
+  end
+  
+  test "the number 9 does not occur anywhere in an encoded number" do
+    [1234, 5678, 1525, 1999].each do |i|
+      assert_no_match /9/, TrainCode.encode(i)
+    end
+  end
+  
+  test "encoded number's sections other than the first are not equal to the input number in base 9" do
+    parts = TrainCode.encode(1234).split("-")
+    parts.shift
+    parts.each do |p|
+      assert_not_equal 1234, p.to_i(9)
+    end
+  end
+  
+  test "encoded number's sections are all different from each other" do
+  end
 end
