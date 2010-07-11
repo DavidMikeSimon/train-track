15720a48dc8a4b083da386b6ac675fbefd16d6c7
DavidMikeSimon <david.mike.simon@gmail.com>
2010-07-11 00:25:11 +0000

    Doing better error correction, and throwing DecodeError when there's not enough information to decode correctly

--- vendor/plugins/train_code/lib/train_code.rb
+++ vendor/plugins/train_code/lib/train_code.rb
@@ -2,6 +2,9 @@ module TrainCode
   class InputError < RuntimeError
   end
   
+  class DecodeError < RuntimeError
+  end
+  
   DOMAIN = 9**4 # No input number >= this can fit in 4 digits of base 9; also each encoded section will be < this
   OFFSETS = [0, 1527, 4872, 3456] # Added to each field modulo DOMAIN
   
@@ -28,6 +31,9 @@ module TrainCode
       p = parts[n].to_i(9) - OFFSETS[n]
       part_scores[p] = part_scores.has_key?(p) ? part_scores[p]+1 : 1
     end
+    
+    scores = part_scores.values.sort.reverse
+    raise DecodeError.new("No clear result") if scores.size > 1 && scores[0] == scores[1]
     
     cand, cand_score = nil, nil
     part_scores.each do |part, score|
--- vendor/plugins/train_code/test/train_code_test.rb
+++ vendor/plugins/train_code/test/train_code_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class TrainCodeTest < ActiveSupport::TestCase
+  def offset_code_section(sec, offset)
+    sec.to_i(9).+(offset).%(TrainCode::DOMAIN).to_s(9)
+  end
+  
   test "can encode a number and get it back after decoding" do
     assert_equal 1234, TrainCode.decode(TrainCode.encode(1234))
   end
@@ -73,20 +77,46 @@ class TrainCodeTest < ActiveSupport::TestCase
     c = TrainCode.encode(1234)
     (0..3).each do |n|
       parts = c.split('-')
-      parts[n] = parts[n].to_i(9).+(1).%(TrainCode::DOMAIN).to_s(9)
+      parts[n] = offset_code_section(parts[n], 15)
       changed_c = parts.join('-')
       assert_equal 1234, TrainCode.decode(changed_c)
     end
   end
   
-  test "can change any two sections of the code and the decoded result will still be correct" do
+  test "can change any two sections of the code by different offsets and the decoded result will still be correct" do
     c = TrainCode.encode(1234)
     [[0,1], [0,2], [0,3], [1,2], [1,3], [2,3]].each do |a, b|
       parts = c.split('-')
-      parts[a] = parts[a].to_i(9).+(1).%(TrainCode::DOMAIN).to_s(9)
-      parts[b] = parts[b].to_i(9).+(1).%(TrainCode::DOMAIN).to_s(9)
+      parts[a] = offset_code_section(parts[a], 38)
+      parts[b] = offset_code_section(parts[b], 45)
       changed_c = parts.join('-')
       assert_equal 1234, TrainCode.decode(changed_c)
+    end
+  end
+  
+  test "after changing any three sections of the code by different offsets will get a decoding exception" do
+    c = TrainCode.encode(1234)
+    (0..3).each do |n|
+      parts = c.split('-')
+      (0..3).each do |m|
+        parts[m] = offset_code_section(parts[m], (m+1)*57) unless n == m
+      end
+      changed_c = parts.join('-')
+      assert_raise TrainCode::DecodeError do
+        TrainCode.decode(changed_c)
+      end
+    end
+  end
+  
+  test "after changing all four sections of the code by different offsets will get a decoding exception" do
+    c = TrainCode.encode(1234)
+    parts = c.split('-')
+    (0..3).each do |m|
+      parts[m] = offset_code_section(parts[m], (m+1)*12)
+    end
+    changed_c = parts.join('-')
+    assert_raise TrainCode::DecodeError do
+      TrainCode.decode(changed_c)
     end
   end
 end
