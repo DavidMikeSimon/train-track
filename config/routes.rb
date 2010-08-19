0d2469a46660921afe2f2afc9f6098884a119a11
David Mike Simon <david.mike.simon@gmail.com>
2010-08-18 22:01:18 -0500

    Added a note about a route i need to remove later

--- config/routes.rb
+++ config/routes.rb
@@ -39,8 +39,8 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
  
-  map.connect 'appointments/:action/:id', :controller => 'appointments'
-  
+  # TODO If the toggle_* actions were web methods, we could remove this manual route
+  map.connect 'appointments/:action/:id', :controller => 'appointments'  
 
   # See how all your routes lay out with "rake routes"
 end
