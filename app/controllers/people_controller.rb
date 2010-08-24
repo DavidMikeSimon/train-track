50f8585b8d12e3c69b0fb5857a935d8190501267
David Mike Simon <david.mike.simon@gmail.com>
2010-08-23 19:56:29 -0500

    Attached People index to fuzzy find

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,5 +3,9 @@ class PeopleController < ApplicationController
   hobo_model_controller
 
   auto_actions :all, :except => [:new, :create]
+  
+  def index
+	hobo_index Person.fuzzy_find_scope(params[:search])
+  end
 
 end
