a291229120e717d242e1aeb417ac156c8fdcb089
David Mike Simon <david.mike.simon@gmail.com>
2010-09-12 22:39:29 -0500

    Now fetching institution information more quickly through another silly hack

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -15,7 +15,6 @@ class PeopleController < ApplicationController
   end
  
   # TODO Should probably just make this format-detected on regular index action
-  # TODO Adjust this once people are more closely associated with an institution
   # TODO Factor out this streaming-CSV-generation idea and have workshop CSV use it too
   index_action :csv do
     filename = "people.csv"
@@ -33,11 +32,10 @@ class PeopleController < ApplicationController
     csv_fields = [
       ["Last Name", lambda { |p| p.last_name }],
       ["First Name", lambda { |p| p.first_name }],
-      #["Institution", lambda { |p| p.appointments.last.institution.name if p.appointments.last }],
-      #["Region", lambda { |p| p.appointments.last.institution.region if p.appointments.last }],
+      ["Institution", lambda { |p| p.institution_name }],
+      ["Region", lambda { |p| p.institution_region }],
       ["Gender", lambda { |p| p.gender }],
-      ["Job", lambda {|p| p.job ? p.job.name : "Other" }],
-      ["Job Details", lambda {|p| p.job_details }]
+      ["Job", lambda {|p| p.job ? p.job.name : "Other" }]
     ]
     
     TrainingSubject.all.each do |ts|
@@ -53,15 +51,19 @@ class PeopleController < ApplicationController
       csv << csv_fields.map {|e| e[0]}
 
       select_fields = ["people.*"]
-      TrainingSubject.all.each do |ts|
+      # TODO Get rid of this silly hack once people are more closely associated with an institution
+      ["name", "region"].each { |institution_field|
+        select_fields << "(SELECT institutions.#{institution_field} FROM appointments LEFT JOIN institutions ON institutions.id = appointments.institution_id LEFT JOIN workshops ON workshops.id = appointments.workshop_id WHERE appointments.person_id = people.id ORDER BY workshops.first_day DESC LIMIT 1) AS institution_#{institution_field}"
+      }
+      TrainingSubject.all.each { |ts|
         select_fields << Person.minute_count_select_expr("%s_minutes" % ts.name, ts)
-      end
+      }
       select_fields << Person.minute_count_select_expr("total_minutes")
 
       # Content
-      Person.all(:select => select_fields.join(","), :order => "last_name, first_name").each do |person|
+      Person.all(:select => select_fields.join(","), :order => "last_name, first_name").each { |person|
         csv << csv_fields.map {|e| (e[1].call(person) || "").to_s}
-      end
+      }
     }
   end
 
